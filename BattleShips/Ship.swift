0e291c72b187e391ee8fa5c062f610536818291e
Hamish <hamisharro@icloud.com>
2021-05-12 14:18:24 +0100

    add testShip() and required tests

---
+++ BattleShips/Ship.swift
@@ -0,0 +1,20 @@
+//
+//  Ship.swift
+//  BattleShips
+//
+//  Created by Hamish Arro on 12/05/2021.
+//
+
+import Foundation
+
+class Ship {
+    var name: String!
+    var size: Int!
+    var direction: Bool!
+    
+    init(name: String, size: Int, direction: Bool) {
+        self.name = name
+        self.size = size
+        self.direction = direction
+    }
+}
---
+++ BattleShipsTests/BattleShipsTests.swift
@@ -0,0 +1,28 @@
+//
+//  BattleShipsTests.swift
+//  BattleShipsTests
+//
+//  Created by Hamish Arro on 12/05/2021.
+//
+
+import XCTest
+@testable import BattleShips
+
+class BattleShipsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testShip() {
+        let smallShip = Ship(name: "Dingy", size: 1, direction: true)
+        XCTAssertEqual(smallShip.name, "Dingy")
+        XCTAssertEqual(smallShip.size, 1)
+        XCTAssertEqual(smallShip.direction, true)
+    }
+
+}
