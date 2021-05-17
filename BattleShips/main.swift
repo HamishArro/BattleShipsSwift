e73181a6cb854e3238d288506e64108d5242d3ec
Hamish <hamisharro@icloud.com>
2021-05-17 16:52:27 +0100

    make separate get direction function

--- BattleShips/main.swift
+++ BattleShips/main.swift
@@ -12,6 +12,7 @@ var ships = [Ship(name: "Destroyer", size: 2), Ship(name: "Submarine", size: 3),
 print("Welcome to battle ships! Please enter your name to begin.")
 let name = String(readLine()!)
 print("Hello \(name), the rules are as follows.\n\n- Players take turns firing shots to attempt to hit the opponent's enemy ships. \n- On your turn, call out a letter (A - H) and a number (1 - 8) that identifies a row and column on your target grid. \n- You will be altered with a 'Hit' or 'Miss' depending on the outcome of the shot.\n")
+
 while !game.gameOver {
     try placeAllShips()
     game.gameOver = true
@@ -20,11 +21,17 @@ while !game.gameOver {
 func placeAllShips() throws {
     for (index, ship) in ships.enumerated() {
         print("\(ship.name!)\nSize: \(ship.size!)\n\nWhere should it face, north or east?")
-        switch String(readLine()!).lowercased() {
-        case "north": ship.direction = true
-        case "east": ship.direction = false
-        default: print("gameOver") }
+        getDriection(ship)
         print("Where do you want to put it?")
         try game.placeShip(String(readLine()!), &ships[index], &game.playerOneGrid)
+    }
+}
+
+func getDriection(_ ship: Ship) {
+    switch String(readLine()!).lowercased() {
+    case "north": ship.direction = true
+    case "east": ship.direction = false
+    default: print("Inncorrect Input, enter again.")
+        getDriection(ship)
     }
 }
